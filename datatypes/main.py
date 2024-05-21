ba824919ba24891a8216a8426990b08c2288f1fc
brian kipkosgei <brianktoo1998@gmail.com>
2024-05-21 22:27:36 +0300

    data types and data type conversion

---
+++ datatypes/main.py
@@ -0,0 +1,13 @@
+num_char= len(input('what is your name: ')) #len(), checks the length of a string
+
+print(type(num_char)) # type(), check the datatype
+print(f'your name has {num_char} has  characters')
+
+# datatye conversion
+value='39'
+first_value=value[0]
+second_value=value[1]
+print(first_value)
+print(type(first_value))
+sum= int(first_value) + int(second_value)
+print(sum)
