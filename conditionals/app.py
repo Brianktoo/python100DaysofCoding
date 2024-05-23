0b413396f6f57625afc726af2dd76265fe5bdac9
brian kipkosgei <brianktoo1998@gmail.com>
2024-05-23 08:24:15 +0300

    added if statement

---
+++ conditionals/app.py
@@ -0,0 +1,10 @@
+# conditional statements
+value = 40
+# if value > 20:
+#     print('value is greater than 20')
+# else:
+#     print('value is not greater than 20')
+
+# ternary operator
+print('value is greater than 20') if value > 20 else print(
+    'value is not! greater than')
