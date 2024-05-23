2f9f073ded1704a330a1672a35a9dd58d0be2fbd
brian kipkosgei <brianktoo1998@gmail.com>
2024-05-23 08:33:36 +0300

    work on string methods

---
+++ datatypes/data_types.py
@@ -0,0 +1,7 @@
+# literal assignments
+firstName = 'bRIAN'
+lastName = 'kipkosgei'
+age = 25
+# string concatination
+fullname = firstName.title() + ' ' + lastName.upper()
+print(f'my name is  {fullname} and age is  {age} years old')
